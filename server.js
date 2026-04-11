b7d7b143703df92cead360e13ef53a9e30e732d7
phquang2001-crypto <phquang2001@gmail.com>
2026-04-11 21:11:09 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// API nhận đặt lịch
+app.post("/api/book", (req, res) => {
+    const data = req.body;
+
+    const bookings = JSON.parse(fs.readFileSync("./data/bookings.json"));
+    bookings.push({
+        ...data,
+        time: new Date()
+    });
+
+    fs.writeFileSync("./data/bookings.json", JSON.stringify(bookings, null, 2));
+
+    res.json({ message: "Đặt lịch thành công!" });
+});
+
+app.listen(PORT, () => {
+    console.log("Server chạy tại http://localhost:" + PORT);
+});
