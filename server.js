edaacb1883098ee754530aeed827ac7b0487d2b2
phquang2001-crypto <phquang2001@gmail.com>
2026-04-13 09:20:39 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -7,23 +7,70 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+const FILE = "./data/bookings.json";
+
+
+app.use("/admin", (req, res, next) => {
+    const auth = { user: "admin", pass: "1324" };
+
+    const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+    const [user, pass] = Buffer.from(b64auth, "base64").toString().split(":");
+
+    if (user === auth.user && pass === auth.pass) {
+        return next();
+    }
+
+    res.set("WWW-Authenticate", 'Basic realm="Admin Area"');
+    res.status(401).send("Authentication required.");
 });
 
-// API nhận đặt lịch
+
+
+
+// API lấy danh sách khách
+app.get("/api/bookings", (req, res) => {
+    const data = JSON.parse(fs.readFileSync(FILE));
+    res.json(data);
+});
+
+// API thêm khách
 app.post("/api/book", (req, res) => {
-    const data = req.body;
+    const bookings = JSON.parse(fs.readFileSync(FILE));
 
-    const bookings = JSON.parse(fs.readFileSync("./data/bookings.json"));
-    bookings.push({
-        ...data,
+    const newBooking = {
+        id: Date.now(),
+        ...req.body,
+        status: "Mới",
         time: new Date()
-    });
+    };
 
-    fs.writeFileSync("./data/bookings.json", JSON.stringify(bookings, null, 2));
+    bookings.push(newBooking);
+    fs.writeFileSync(FILE, JSON.stringify(bookings, null, 2));
 
     res.json({ message: "Đặt lịch thành công!" });
+});
+
+// API xoá
+app.delete("/api/book/:id", (req, res) => {
+    let bookings = JSON.parse(fs.readFileSync(FILE));
+    bookings = bookings.filter(b => b.id != req.params.id);
+    fs.writeFileSync(FILE, JSON.stringify(bookings, null, 2));
+    res.json({ message: "Đã xoá" });
+});
+
+// API cập nhật trạng thái
+app.put("/api/book/:id", (req, res) => {
+    let bookings = JSON.parse(fs.readFileSync(FILE));
+
+    bookings = bookings.map(b => {
+        if (b.id == req.params.id) {
+            return { ...b, status: req.body.status };
+        }
+        return b;
+    });
+
+    fs.writeFileSync(FILE, JSON.stringify(bookings, null, 2));
+    res.json({ message: "Đã cập nhật" });
 });
 
 app.listen(PORT, () => {
